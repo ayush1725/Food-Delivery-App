d148d0a5999c95410e4465c81a7ebcdbb2841f3f
Ayush <ayushdwibedy123@gmail.com>
2024-12-28 23:46:28 +0530

    created build

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import Placeorder from './pages/PlaceOrder/Placeorder'
-import Footer from './components/Footer/Footer'
-import { useState } from 'react'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-const App = () => {
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-  const [showLogin,setShowLogin] = useState(false)
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import Placeorder from './pages/PlaceOrder/Placeorder';
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-    {showLogin? <LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-      <Navbar setShowLogin={setShowLogin} />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<Placeorder/>}/>
-      </Routes>
-    </div>
-    <Footer/>
-  </>
-  )
-}
+      <ToastContainer />
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
--- Client/src/components/FoodItem/FoodItem.jsx
+++ Client/src/components/FoodItem/FoodItem.jsx
@@ -3,6 +3,7 @@ import './FoodItem.css';
 import { assets } from '../../assets/assets';
 import { StoreContext } from '../../Context/StoreContext';
 
+
 const FoodItem = ({ id, name, price, description, image }) => {
     const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
 
